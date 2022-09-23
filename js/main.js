567e5d665a0cd78b74bb006618a2c81bfd884c80
maciej holiszowski <matias06112001@gmail.com>
2022-09-23 13:17:22 +0200

    More appLogos images, adjusted body and shop size

--- js/main.js
+++ js/main.js
@@ -1,15 +1,84 @@
 
+// Recogemos el ID del boton que usaremos para vaciar y le añadimos un EventListener.
+// Importante hacerlo con un callback a la función
+let botonVaciar = document.getElementById("botonVaciar");
+botonVaciar.addEventListener("click", () => { vaciarCarrito() });
+
+
+//Recogemos el Id del elemento carrito para poder interactuar con el mediante el DOM
+
+
 let totalCarrito = document.getElementById("totalCarrito");
 let precioTotal = 0;
 totalCarrito.innerHTML = `${precioTotal} MB`;
 
+
+// Declaramos un array con objetos que utilizaremos en nuestra "tienda"
 let objetos = [
     {
         id: "item1",
         pesoMb: 405,
     },
-    {},
-    {}
+    {
+        id: "item2",
+
+    },
+    {
+        id: "item3",
+
+    },
+    {
+        id: "item4",
+
+    },
+    {
+        id: "item5",
+
+    },
+    {
+        id: "item6",
+
+    },
+    {
+        id: "item7",
+
+    },
+    {
+        id: "item8",
+
+    },
+    {
+        id: "item9",
+
+    },
+    {
+        id: "item10",
+
+    },
+    {
+        id: "item11",
+
+    },
+    {
+        id: "item12",
+
+    },
+    {
+        id: "item13",
+
+    },
+    {
+        id: "item14",
+
+    },
+    {
+        id: "item15",
+
+    },
+    {
+        id: "item16",
+
+    }
 ];
 
 
@@ -52,4 +121,17 @@ const drop = (ev) => {
     precioTotal += objetoDeseo.pesoMb;
 
     totalCarrito.innerHTML = `${precioTotal} MB`;
+
+    if (precioTotal > 1024) {
+
+        let precioTotalGB = precioTotal / 1024;
+        totalCarrito.innerHTML = `${precioTotalGB.toFixed(2)} GB`;
+    }
 };
+
+
+const vaciarCarrito = () => {
+    precioTotal = 0;
+    totalCarrito.innerHTML = `${precioTotal} MB`;
+
+}
